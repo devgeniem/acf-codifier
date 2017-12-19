510469fc0d9a2684254f561b14d9bab02e526312
Miika Arponen <miika@geniem.com>
2017-12-19 16:39:12 +0200

    Added a check for unique field keys

--- src/Field.php
+++ src/Field.php
@@ -59,11 +59,18 @@ abstract class Field {
     protected $default_value;
 
     /**
-     * Filters and actions to be hooked
+     * Filters and actions to be hooked.
      *
      * @var array
      */
     protected $filters = [];
+
+    /**
+     * Store registered field keys to warn if there are duplicates.
+     *
+     * @var [type]
+     */
+    static protected $keys = [];
 
     /**
      * Constructor.
@@ -85,15 +92,44 @@ abstract class Field {
 
         $this->label = $label;
 
-        $this->key = $key ?? sanitize_title( $label );
+        $this->inner_set_key( $key ?? $label );
 
         $this->name = $name ?? sanitize_title( $label );
 
         $this->wrapper = [
             'width' => '',
             'class' => '',
-            'id' => '',
+            'id'    => '',
         ];
+    }
+
+    /**
+     * A private function to set the field key.
+     * Sanitizes the given string first, and
+     * checks its uniqueness too.
+     *
+     * @param string $key The key to set.
+     * @return void
+     */
+    private function inner_set_key( $key ) {
+        $key = sanitize_title( $key );
+
+        if ( ! isset( self::$keys[ $key ] ) ) {
+            $this->key = $key;
+
+            // Save backtrace data if we want to debug.
+            if ( WP_DEBUG === true ) {
+                $debug_backtrace    = debug_backtrace();
+                self::$keys[ $key ] = ' which was defined in ' . $debug_backtrace[1]['file'] . ' at line ' . $debug_backtrace[1]['line'];
+            }
+            // Otherwise just save the info that this is not right.
+            else {
+                self::$keys[ $key ] = '';
+            }
+        }
+        else {
+            trigger_error( 'ACF Codifier: field key "' . $key . '" is already in use within another field' . self::$keys[ $key ] . '.', E_USER_NOTICE );
+        }
     }
 
     /**

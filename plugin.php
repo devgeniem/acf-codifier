a696546918bc86f98ba042561b533ad8550c8637
Miika Arponen <miika@geniem.com>
2018-01-30 14:23:23 +0200

    Clone field now throws an exception if a conditional logic is tried to be applied to it

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: ACF Codifier
 Plugin URI: https://github.com/devgeniem/acf-codifier
 Description: A helper class to make defining ACF field groups and fields easier in the code.
-Version: 1.2.2
+Version: 1.2.3
 Author: Geniem Oy
 Author URI: https://geniem.fi
 License: GPLv2

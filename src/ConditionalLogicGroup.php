0df5d1e9b23c223d3481f2b4029001a6ed918e84
Miika Arponen <miika@geniem.com>
2020-06-16 12:28:27 +0300

    Additional operators for conditional logic field

--- src/ConditionalLogicGroup.php
+++ src/ConditionalLogicGroup.php
@@ -20,14 +20,14 @@ class ConditionalLogicGroup {
      * Add a conditional logic rule.
      *
      * @param mixed  $field         The slug of the field to be compared to.
-     * @param string $operator      Operator to be used, either '==' or '!='.
+     * @param string $operator      Operator to be used. Possibilities: '==', '!=', '!=empty', '==empty', '==pattern', '==contains', '<' or '>'.
      * @param mixed  $value         Value to be used in comparison.
      * @throws \Geniem\ACF\Exception Throw error if given parameter is not valid.
      * @return self
      */
     public function add_rule( $field, string $operator, $value ) {
         // Check for valid values for the parameters.
-        if ( ! in_array( $operator, [ '==', '!=' ], true ) ) {
+        if ( ! in_array( $operator, [ '==', '!=', '!=empty', '==empty', '==pattern', '==contains', '<', '>' ], true ) ) {
             throw new \Geniem\ACF\Exception( 'Geniem\ACF\ConditionalRule: add_role() does not accept argument "' . $operator . '"' );
         }
         if ( ! ( $field instanceof \Geniem\ACF\Field || is_string( $field ) ) ) {

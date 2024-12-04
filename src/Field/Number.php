8d5850fef5474045fab88084780c0f19aba4a5f9
Jaakko Lehtonen <jaakko.lehtonen@cap.fi>
2024-12-04 13:34:13 +0200

    Refactor set_step method to support float step sizes and enforce positive values

--- src/Field/Number.php
+++ src/Field/Number.php
@@ -42,10 +42,20 @@ class Number extends \Geniem\ACF\Field {
     /**
      * Set step size
      *
-     * @param integer $step Step size.
+     * @param integer|float $step Step size.
      * @return self
      */
-    public function set_step( int $step ) {
+    public function set_step( $step ) {
+        // Validate that the step is numeric
+        if ( ! is_numeric( $step ) ) {
+            throw new \Geniem\ACF\Exception( 'Geniem\ACF\Field\Number: set_step() only accepts numeric values' );
+        }
+
+        // Ensure the step is positive
+        if ( $step <= 0 ) {
+            throw new \Geniem\ACF\Exception( 'Geniem\ACF\Field\Number: set_step() only accepts positive values' );
+        }
+
         $this->step = $step;
 
         return $this;
@@ -54,7 +64,7 @@ class Number extends \Geniem\ACF\Field {
     /**
      * Get step size
      *
-     * @return integer
+     * @return integer|float
      */
     public function get_step() {
         return $this->step;
